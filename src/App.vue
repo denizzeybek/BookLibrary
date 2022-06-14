36767817fcf3f8cada8586812556899bb899edcc
Deniz Zeybek <46136181+denizzeybek@users.noreply.github.com>
2022-06-14 18:03:47 +0300

    Tailwind implemented

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,13 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <p class="text-3xl text-red-900">araba</p>
     </nav>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style>
+/* @import "./assets/tailwind.css"; */
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
